04f159337b369a8d76e3b025ee5f055fdeff1d91
77nimesh <77nimesh@gmail.com>
2025-09-07 23:39:13 +0930

    edit assemblyInfo

--- ITWorks.Enrollment.ConsoleApp/Properties/AssemblyInfo.cs
+++ ITWorks.Enrollment.ConsoleApp/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ITWorks.Enrolment.ConsoleApp")]
+[assembly: AssemblyTitle("ITWorks.Enrollment.ConsoleApp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ITWorks.Enrolment.ConsoleApp")]
+[assembly: AssemblyProduct("ITWorks.Enrollment.ConsoleApp")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
